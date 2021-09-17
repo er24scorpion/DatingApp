8e1f409bd2463089818ce2cb88db7a52614523e8
ekaterinarusanova <katya@hoteliga.com>
2021-09-17 22:10:08 +0200

    End of section 14

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,15 +1,37 @@
+import { Pagination } from 'src/app/models/pagination';
+import { MembersService } from './../_services/members.service';
+import { Member } from 'src/app/_modules/member';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+  styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent implements OnInit {
+  members: Partial<Member[]> = [];
+  predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
+  pagination!: Pagination;
 
-  constructor() { }
+  constructor(private membersService: MembersService) {}
 
   ngOnInit(): void {
+    this.loadLikes();
   }
 
+  loadLikes() {
+    this.membersService
+      .getLikes(this.predicate, this.pageNumber, this.pageSize)
+      .subscribe((response) => {
+        this.members = response.result;
+        this.pagination = response.pagination;
+      });
+  }
+
+  pageChanged(event: any) {
+    this.pagination = event.page;
+    this.loadLikes();
+  }
 }
--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr';
+import { MembersService } from './../../_services/members.service';
 import { Member } from './../../_modules/member';
 import { Component, Input, OnInit } from '@angular/core';
 
@@ -9,7 +11,16 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MemberCardComponent implements OnInit {
   @Input() member!: Member;
 
-  constructor() {}
+  constructor(
+    private membersService: MembersService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
+
+  addLike(member: Member) {
+    this.membersService
+      .addLike(member.username)
+      .subscribe(() => this.toastr.success('You have liked ' + member.knownAs));
+  }
 }
